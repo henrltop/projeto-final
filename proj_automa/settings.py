724040b544c9c5ec3b65717a49a807a9806c1f06
henrique <henriquesevdacunha@gmail.com>
2024-09-27 17:48:37 -0400

    autorizando os IPs locais

--- proj_automa/settings.py
+++ proj_automa/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b2b_(acz&%ji(=*!39#*jbsy8_gy8=h)cy)@kl^gp9ta+@ka4v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.102', 'localhost', '127.0.0.1']
 
 
 # Application definition
